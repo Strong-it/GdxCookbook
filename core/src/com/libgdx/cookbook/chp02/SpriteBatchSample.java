193dc05191f02ad64e1b58a83e3bc986d7ce8310
strong_it <zqiang.077@gmail.com>
2015-12-04 11:32:43 +0800

    增加更多的cavemen测试位置旋转 放大 以及设置颜色

--- core/src/com/libgdx/cookbook/chp02/SpriteBatchSample.java
+++ core/src/com/libgdx/cookbook/chp02/SpriteBatchSample.java
@@ -1,6 +1,7 @@
 package com.libgdx.cookbook.chp02;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -14,6 +15,7 @@ public class SpriteBatchSample extends BaseScreen {
 	final String TAG = SpriteBatchSample.class.getSimpleName();
 	
 	private Texture cavemanTexture;
+	private Color oldColor;
 	
 	private int width, height;
 	private float originX, originY;
@@ -26,13 +28,16 @@ public class SpriteBatchSample extends BaseScreen {
 		batch = new SpriteBatch();
 		
 		cavemanTexture = new Texture(Gdx.files.internal("data/caveman.png"));
-		cavemanTexture.setFilter(TextureFilter.Nearest, TextureFilter.Nearest);
+		cavemanTexture.setFilter(TextureFilter.Nearest, TextureFilter.Nearest);  // Nearest 比较接近原始图像，描述坚硬物体
+																				 // Linear 修改周围像素产生模糊图像， 描述羽毛
+		oldColor = new Color();
 		
 		width = cavemanTexture.getWidth();
 		height = cavemanTexture.getHeight();
 		
 		originX = width * 0.5f;
 		originY = height * 0.5f;
+		Gdx.app.log(TAG, ""+originX);
 	}
 
 	@Override
@@ -44,15 +49,65 @@ public class SpriteBatchSample extends BaseScreen {
 		debugTool.drawGrid(SCREEN_WIDTH, SCREEN_HEIGHT);  // 画出表格来确定人物所在的位置
 		
 		batch.begin();
-		batch.draw(cavemanTexture,   // texture本身
-					-originX, -originY,  // word space coordinates,, considering camera centered at(0, 0)
-					originX, originY,   // coordinates in pixels of our texture
-					width , height,     // 人物在屏幕上画出的宽度，和高度，如果width / 2,那么人物的宽度会减半，但是仍然弄画出全部的人物
+		// Render caveman centered on screen
+		batch.draw(cavemanTexture,   // texture本身        // Texture
+					-originX, -originY,  // word space coordinates,, considering camera centered at(0, 0) //  x, y
+					originX, originY,   // coordinates in pixels of our texture  // originX, originY
+					width , height,     // 人物在屏幕上画出的宽度，和高度，如果width / 2,那么人物的宽度会减半，但是仍然弄画出全部的人物  // width, height
 					WORLD_TO_SCREEN, WORLD_TO_SCREEN,  // scaleX, scaleY
-					0f,      // rot(degress)  是否旋转
+					0f,      // rotation(degress)  是否旋转
 					0, 0,      // srcX, srcY  Texture本身的起始位置
-					width, height,  // Texture的宽和高,如果width / 2,那么人物只能画出一般
+					width, height,  // Texture的宽和高,如果width / 2,那么人物只能画出一般  //scrwidth, srcHeight
 					false, false);  //flipX, flipY  是否X轴翻转， 是否Y轴翻转
+		
+		// Render caveman on the top left corner at 2x size
+		batch.draw(cavemanTexture, 
+					-4.0f - originX, 1.5f -originY, // -originX, -originY 是一个相对坐标，其余的位置都要在这个2个点上加减
+					originX, originY,
+					width, height, 
+					WORLD_TO_SCREEN * 2.0f, WORLD_TO_SCREEN * 2.0f,
+					0.0f, 
+					0, 0,
+					width, height,
+					false, false);
+		
+		// Render caveman on the bottom left corner at 0.5x size
+		batch.draw(cavemanTexture,
+				-4.0f - originX, -1.5f - originY,
+				originX, originY,
+				width, height,
+				WORLD_TO_SCREEN * 0.5f, WORLD_TO_SCREEN * 0.5f, 
+				0.0f,
+				0, 0, 
+				width, height, 
+				false, false);
+		
+		// Render caveman on the top right corner at 2x size and flipped around X and Y
+		batch.draw(cavemanTexture,
+				4.0f - originX, 1.5f - originY,
+				originX, originY,
+				width, height,
+				WORLD_TO_SCREEN * 2f, WORLD_TO_SCREEN * 2f, 
+				0.0f,
+				0, 0, 
+				width, height, 
+				true, true);
+		
+		oldColor = batch.getColor();
+		
+		batch.setColor(Color.RED);
+		// Render red caveman
+		batch.draw(cavemanTexture,
+				-originX, 2.0f - originY,
+				originX, originY,
+				width, height,
+				WORLD_TO_SCREEN, WORLD_TO_SCREEN, 
+				0.0f,
+				0, 0, 
+				width, height, 
+				false, false);
+		
+		batch.setColor(oldColor);
 		debugTool.operateCamera();
 		batch.end();
 		
