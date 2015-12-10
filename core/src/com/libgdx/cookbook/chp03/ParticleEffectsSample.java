5b5c73da27d8722fc2007536105aaf2cc4d9ba77
Strong_it <zqiang.077@gmail.com>
2015-12-10 16:49:01 +0800

    增加ParticleEffectPool的测试用例

--- core/src/com/libgdx/cookbook/chp03/ParticleEffectsSample.java
+++ core/src/com/libgdx/cookbook/chp03/ParticleEffectsSample.java
@@ -4,14 +4,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
+import com.badlogic.gdx.graphics.g2d.ParticleEffectPool.PooledEffect;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.libgdx.cookbook.help.BaseScreen;
 
 public class ParticleEffectsSample extends BaseScreen {
 
-    private ParticleEffect[ ] effects;
+    private ParticleEffect[] effects;
+    private ParticleEffectPool pool;
+    private Array<PooledEffect> activeEffects;
     private int currentEffect;
     private Vector3 touchPos;
     
@@ -40,6 +45,11 @@ public class ParticleEffectsSample extends BaseScreen {
         
         Gdx.input.setInputProcessor(this);  // 其实BaseScreen可以不继承InputAdapter,在类内部声明一个 InputAdapter inputAdapter
                                             // Gdx.input.setInputProcessor(inputAdapter) 但此时是内部类
+        
+        ParticleEffect explosionEffect = new ParticleEffect();
+        explosionEffect.load(Gdx.files.internal("data/explosion.particle"), Gdx.files.internal("data"));
+        pool = new ParticleEffectPool(explosionEffect, 10, 100);
+        activeEffects = new Array<PooledEffect>();
     }
     
     @Override
@@ -47,8 +57,8 @@ public class ParticleEffectsSample extends BaseScreen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
-        touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-        camera.unproject(touchPos);   // screen坐标和world坐标不是对应的，比如scal的时候，这时候需要将屏幕坐标转换到world坐标
+//        touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+//        camera.unproject(touchPos);   // screen坐标和world坐标不是对应的，比如scal的时候，这时候需要将屏幕坐标转换到world坐标
         
         for (ParticleEffect particleEffect : effects) {
             particleEffect.setPosition(touchPos.x, touchPos.y);
@@ -57,10 +67,25 @@ public class ParticleEffectsSample extends BaseScreen {
                 particleEffect.reset();  // 粒子生命周期结束，自动重置，这样就能不断刷新
             }
         }
+        
         batch.setProjectionMatrix(camera.combined);
         
         batch.begin();
+        //测试ParticleEffectPool用例暂时屏蔽掉
         effects[currentEffect].draw(batch, delta);  // 每个delta时间就刷新一次
+        
+        
+        for (int i = 0; i < activeEffects.size; ) {
+            PooledEffect effect = activeEffects.get(i);
+            
+            if (effect.isComplete()) {
+                pool.free((PooledEffect) effect);
+                activeEffects.removeIndex(i);
+            } else {
+                effect.draw(batch, delta);
+                ++i;
+            }
+        }
         batch.end();
     }
     
@@ -81,6 +106,16 @@ public class ParticleEffectsSample extends BaseScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         currentEffect = (currentEffect + 1) % effects.length;
+        
+        PooledEffect effect = pool.obtain();
+        
+        if (effect != null) {
+            touchPos.set(screenX, screenY, 0.0f);
+            camera.unproject(touchPos);
+            
+            activeEffects.add(effect);
+            effect.setPosition(touchPos.x, touchPos.y);
+        }
         return true;
     }
     
